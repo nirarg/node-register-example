c20ea1382b7c742cf2eafe707adcdbbaac12e1f1
Nir Argaman <nir@dhcp-1-67.tlv.redhat.com>
2020-02-12 17:24:41 +0200

    Add operator with pods initialize

---
+++ app-operator/pkg/apis/app/v1alpha1/noderegister_types.go
@@ -0,0 +1,46 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// NodeRegisterSpec defines the desired state of NodeRegister
+type NodeRegisterSpec struct {
+	// Size is the size of the nodeRegister deployment
+	Size int32 `json:"size"`
+}
+
+// NodeRegisterStatus defines the observed state of NodeRegister
+type NodeRegisterStatus struct {
+	// Nodes are the names of the nodeRegister pods
+	Nodes []string `json:"nodes"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NodeRegister is the Schema for the noderegisters API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=noderegisters,scope=Namespaced
+type NodeRegister struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   NodeRegisterSpec   `json:"spec,omitempty"`
+	Status NodeRegisterStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// NodeRegisterList contains a list of NodeRegister
+type NodeRegisterList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []NodeRegister `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&NodeRegister{}, &NodeRegisterList{})
+}
---
+++ app-operator/pkg/controller/noderegister/noderegister_controller.go
@@ -0,0 +1,241 @@
+package noderegister
+
+import (
+	"context"
+	"reflect"
+
+	"github.com/go-logr/logr"
+
+	appv1alpha1 "github.com/example-inc/app-operator/pkg/apis/app/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_noderegister")
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new NodeRegister Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileNodeRegister{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("noderegister-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource NodeRegister
+	err = c.Watch(&source.Kind{Type: &appv1alpha1.NodeRegister{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create that are owned by the primary resource
+	// Watch for changes to secondary resource Pods and requeue the owner NodeRegister
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &appv1alpha1.NodeRegister{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileNodeRegister implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileNodeRegister{}
+
+// ReconcileNodeRegister reconciles a NodeRegister object
+type ReconcileNodeRegister struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a NodeRegister object and makes changes based on the state read
+// and what is in the NodeRegister.Spec
+// TODO(user): Modify this Reconcile function to implement your Controller logic.  This example creates
+// a Pod as an example
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileNodeRegister) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling NodeRegister")
+
+	reqLogger.Info("Fetch the NodeRegister instance")
+	nodeRegister := &appv1alpha1.NodeRegister{}
+	reqLogger.Info("Get nodeRegister instance")
+	err := r.client.Get(context.TODO(), request.NamespacedName, nodeRegister)
+	if err != nil {
+		reqLogger.Info("nodeRegister instance not found. Ignoring since object must be deleted")
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		reqLogger.Error(err, "Failed to get nodeRegister instance")
+		return reconcile.Result{}, err
+	}
+
+	reqLogger.Info("Check if the deployment already exists, if not create a new one")
+	found := &appsv1.Deployment{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: nodeRegister.Name, Namespace: nodeRegister.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		reqLogger.Info("Deployment is not found")
+		dep := r.deploymentForNodeRegister(nodeRegister, reqLogger)
+		reqLogger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.client.Create(context.TODO(), dep)
+		if err != nil {
+			reqLogger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("Deployment created successfully - return and requeue")
+		return reconcile.Result{Requeue: true}, nil
+	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Deployment")
+		return reconcile.Result{}, err
+	}
+
+	reqLogger.Info("Ensure the deployment size is the same as the spec")
+	size := nodeRegister.Spec.Size
+	if *found.Spec.Replicas != size {
+		found.Spec.Replicas = &size
+		err = r.client.Update(context.TODO(), found)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
+			return reconcile.Result{}, err
+		}
+		reqLogger.Info("Spec updated - return and requeue")
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	reqLogger.Info("Update the NodeRegister status with the pod names")
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(nodeRegister.Namespace),
+		client.MatchingLabels(labelsForNodeRegister(nodeRegister.Name)),
+	}
+	if err = r.client.List(context.TODO(), podList, listOpts...); err != nil {
+		reqLogger.Error(err, "Failed to list pods", "nodeRegister.Namespace", nodeRegister.Namespace, "nodeRegister.Name", nodeRegister.Name)
+		return reconcile.Result{}, err
+	}
+	podNames := getPodNames(podList.Items)
+
+	reqLogger.Info("Update status.Nodes if needed")
+	if !reflect.DeepEqual(podNames, nodeRegister.Status.Nodes) {
+		nodeRegister.Status.Nodes = podNames
+		err := r.client.Status().Update(context.TODO(), nodeRegister)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update nodeRegister status")
+			return reconcile.Result{}, err
+		}
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// deploymentForNodeRegister returns a nodeRegister Deployment object
+func (r *ReconcileNodeRegister) deploymentForNodeRegister(nr *appv1alpha1.NodeRegister, reqLogger logr.Logger) *appsv1.Deployment {
+	ls := labelsForNodeRegister(nr.Name)
+	replicas := nr.Spec.Size
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      nr.Name,
+			Namespace: nr.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image:           "node-register-server",
+						Name:            nr.Name + "-pod",
+						ImagePullPolicy: "Never",
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: 8080,
+						}},
+					}},
+				},
+			},
+		},
+	}
+	reqLogger.Info("Set NodeRegister instance as the owner and controller")
+	controllerutil.SetControllerReference(nr, dep, r.scheme)
+	return dep
+}
+
+// labelsForMemcached returns the labels for selecting the resources
+// belonging to the given memcached CR name.
+func labelsForNodeRegister(name string) map[string]string {
+	return map[string]string{"app": name + "-pod", "noderegister_cr": name}
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
+}
+
+// newPodForCR returns a busybox pod with the same name/namespace as the cr
+func newPodForCR(cr *appv1alpha1.NodeRegister) *corev1.Pod {
+	labels := map[string]string{
+		"app": cr.Name,
+	}
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      cr.Name + "-pod",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:    "busybox",
+					Image:   "busybox",
+					Command: []string{"sleep", "3600"},
+				},
+			},
+		},
+	}
+}
